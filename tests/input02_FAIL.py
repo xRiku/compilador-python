0a4ffc906d71edcd3403c8b13cc58f9ba0900491
João Felipe Gobeti Calenzani <joao.gobeti@motora.ai>
2021-07-21 19:42:35 -0300

    mudança em alguns arquivos de testes

--- tests/input02_FAIL.py
+++ tests/input02_FAIL.py
@@ -1,5 +1,5 @@
 def teste7():
-    names = (1,2,3,"teste")
+    names = (1,2,3,"teste)
     for name in names:
         print(name)
     for i in [1,2,3]:
--- tests/input03_FAIL.py
+++ tests/input03_FAIL.py
@@ -1,6 +1,6 @@
 def soma2(y):
     y = lambda (x : x+2)
-    resposta = y(2)
+    resposta = y(2);
     return y
 
 meuitem = 3
